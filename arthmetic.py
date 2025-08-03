732015f24ae15a42432594da8762ac2088497cef
Huzaifa1290 <huzaifakhalid1290@gmail.com>
2025-08-03 19:17:33 +0500

    Uploaded Python assignment with screenshots

---
+++ arthmetic.py
@@ -0,0 +1,14 @@
+a = 15
+b = 4
+
+print("Addition: a + b =", a + b)
+print("Subtraction: a - b =", a - b)
+print("Multiplication: a * b =", a * b)
+print("Division: a / b =", a / b)
+print("Floor Division: a // b =", a // b)
+print("Modulus: a % b =", a % b)
+print("Exponentiation: a ** b =", a ** b)
+
+# Q: What do you expect for floor division and modulus?
+# Floor division:   3
+# Modulus:  3
